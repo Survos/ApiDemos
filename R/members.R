ffc4180e4af70cd839cee06c0c10fe56fd83a73c
Phill <phill@starkingdom.co.uk>
2015-09-12 11:59:59 +0000

    First commit of new members endpoint function.

---
+++ R/members.R
@@ -0,0 +1,13 @@
+members <- function(projectCode, maxPerPage = "25", pii = "0"){
+  
+  # Set function options
+  
+  funcOps <<- paste("project_code=",projectCode, "&max_per_page=", maxPerPage, "&PII=", pii, sep="")
+  
+  # Define the API endpoint extension to match this function and any other endpoint params
+  
+  dataReturn <- endPointParams(endPointExt="members?", endPointPages="&page=")
+  
+  dplyr::bind_rows(dataReturn[1:length(dataReturn)])
+  
+}
