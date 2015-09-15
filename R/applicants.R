7160267833545255747e2afe0607e646845d99be
Phill <phill@starkingdom.co.uk>
2015-09-15 21:33:26 +0000

    Updated applicants R to new end point

--- R/applicants.R
+++ R/applicants.R
@@ -1,20 +1,10 @@
-applicants <- function(maxPerPage = "25"){
+applicants <- function(projectCode, maxPerPage = "25", pii = "0"){
   
   # Set function options
-  funcOps <<- paste("max_per_page=", maxPerPage, sep="")
+  funcOps <<- paste("project_code=",projectCode, "&max_per_page=", maxPerPage, "&PII=", pii, "&criteria[enrollment_status_code]=applicant", sep="")
   
   # Define the API endpoint extension to match this function and any other endpoint params
   
-  dataReturn <- endPointParams(endPointExt="members/applicants?", endPointPages="&page=")
-  
-  # Make the return pretty by un-nesting.
-  
-  firstFiveCols <- as.data.frame(dataReturn)
-  
-  lastTwoCols <- as.data.frame(firstFiveCols$submitted)
-  
-  firstFiveCols <- firstFiveCols[,c(1:5)]
-  
-  cbind(firstFiveCols,lastTwoCols)
+  endPointParams(endPointExt="members?", endPointPages="&page=")
 
 }
--- R/endPointParams.R
+++ R/endPointParams.R
@@ -15,7 +15,13 @@ endPointParams <- function(endPointExt, endPointPages){
   
   getData <- httr::GET(url=url, add_headers(Authorization=paste("Bearer ", accessToken, sep="")))
   
+  if (httr::status_code(getData[[1]]) != "200") {
+    stop("Status Code: ", httr::status_code(getData[[1]]), " Something went wrong. Please check URL validity. ", url[1])
+  } else {
+    
   dataReturn <- jsonlite::fromJSON(content(getData,type="text"))
+  
+  }
   
   # Everything below here deals with returning data across all pages. 
   
