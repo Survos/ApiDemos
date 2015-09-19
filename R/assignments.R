b75525140572f3b4d4559df12a57829086f6a29c
Phill <phill@starkingdom.co.uk>
2015-09-19 11:59:13 +0000

    Updated entire function to add additional optional arguments

--- R/assignments.R
+++ R/assignments.R
@@ -1,13 +1,18 @@
-assignments <- function(jobId, maxPerPage = "25"){
+assignments <- function(maxPerPage, jobId, projectCode, waveId){
   
-  # Set function options
+  # Set function options, accounting for missing options
+  ifelse(missing(maxPerPage), maxPerPage <- paste("max_per_page=25"), maxPerPage <- paste("max_per_page=", maxPerPage, sep=""))
   
-  funcOps <<- paste("job_id=", jobId, "&max_per_page=", maxPerPage, sep="")
+  ifelse(missing(jobId), jobId <- NA, jobId <- paste("&job_id=", jobId, sep=""))
+  ifelse(missing(projectCode), projectCode <- NA, projectCode <- paste("&project_code=", projectCode, sep=""))
+  ifelse(missing(waveId),  waveId <- NA, waveId <- paste("&wave_id=", waveId, sep=""))
+  
+  optsString <- ifelse(is.na(jobId), maxPerPage, paste(maxPerPage, jobId, sep=""))
+  optsString <- ifelse(is.na(projectCode), optsString, paste(optsString, projectCode, sep=""))
+  funcOps <<- ifelse(is.na(waveId), optsString, paste(optsString, waveId, sep=""))
   
   # Define the API endpoint extension to match this function and any other endpoint params
   
- endPointParams(endPointExt="assignments?", endPointPages="&page=")
+  endPointParams(endPointExt="assignments?", endPointPages="&page=")
   
-  #dplyr::bind_rows(dataReturn[1:length(dataReturn)])
-
 }
